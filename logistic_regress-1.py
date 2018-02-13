3ec59cc672e05f958f782a5cff0e5844b6320ee9
Parvez Kose <flamboyanz@gmail.com>
2018-02-12 20:33:17 -0500

    logisitc regression with python to test churn prediction on customer data

---
+++ logistic_regress-1.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn import linear_model, datasets
+from sklearn.cross_validation import train_test_split
