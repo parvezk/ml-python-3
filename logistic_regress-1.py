e1e05056d011676a3548f6275dd1965b03d0bf3a
Parvez Kose <flamboyanz@gmail.com>
2018-02-13 12:06:52 -0500

    update

--- logistic_regress-1.py
+++ logistic_regress-1.py
@@ -5,3 +5,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn import linear_model, datasets
 from sklearn.cross_validation import train_test_split
+
+data = pd.read_csv('train.csv', header=0)
+print(data.shape)
+print(list(data.columns))
