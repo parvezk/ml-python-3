7a6f7b8a26fe092a759e6d298d78ba02255be0ae
Parvez Kose <flamboyanz@gmail.com>
2018-02-14 23:37:48 -0500

    update day coding

--- logistic_regress-1.py
+++ logistic_regress-1.py
@@ -9,3 +9,9 @@ from sklearn.cross_validation import train_test_split
 data = pd.read_csv('train.csv', header=0)
 print(data.shape)
 print(list(data.columns))
+
+features = list(data.columns[:11])
+print(features)
+Y = data["LEAVE"]
+X = data[features]
+h = .02  # step size in the mesh
